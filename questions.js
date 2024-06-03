f04c15f5e2f14c8475da05a5076ab4e84eb3a7d0
AceSolleza <113578060+AceSolleza@users.noreply.github.com>
2024-06-03 09:32:56 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,67 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Guess the Betta Fish Breed",
+    image: "images/img6.jpg",
+    answer: "Veiltail Betta",
+    options: [
+      "Combtail",
+      "Super Delta",
+      "Veiltail Betta",
+      "Crowntail"
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "Guess the Betta Fish Breed",
+    image: "images/img7.png",
+    answer: "Crowntail",
+    options: [
+      "Super Delta",
+      "Crowntail",
+      "Veiltail Betta",
+      "Combtail"
+    ]
+  },
+
+  {
+    numb: 3,
+    question: "Guess the Betta Fish Breed",
+    image: "images/img8.jpg",
+    answer: "Spade Tail",
+    options: [
+      "Crowntail",
+      "Spade Tail",
+      "Combtail",
+      "Super Delta"
+    ]
+  },
+
+  {
+    numb: 4,
+    question: "Guess the Betta Fish Breed",
+    image: "images/img9.jpg",
+    answer: "Rosetail",
+    options: [
+      "Crowntail",
+      "Rosetail",
+      "Spade Tail",
+      "Super Delta"
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "Guess the Betta Fish Breed",
+    image: "images/img10.jpg",
+    answer: "Super Delta",
+    options: [
+      "Crowntail",
+      "Rosetail",
+      "Super Delta",
+      "Combtail"
+    ]
+  },
+
+];
